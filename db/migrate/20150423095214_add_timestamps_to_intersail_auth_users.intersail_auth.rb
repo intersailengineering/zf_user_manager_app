fc9b8e098dbd0feaf739a7eb64bb75568ba0bb4d
Andrea Salomoni <andrea.salomoni@intersail.it>
2015-04-27 16:59:19 +0200

    db setup

--- db/migrate/20150423095214_add_timestamps_to_intersail_auth_users.intersail_auth.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from intersail_auth (originally 20150326165104)
-class AddTimestampsToIntersailAuthUsers < ActiveRecord::Migration
-  def change
-    add_timestamps(:intersail_auth_users)
-  end
-end
