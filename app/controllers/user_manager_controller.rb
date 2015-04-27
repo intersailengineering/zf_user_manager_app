c728a88e611fabc9704f7c6dfe2cc74182ee8c18
Andrea Salomoni <andrea.salomoni@intersail.it>
2015-04-27 12:47:09 +0200

    first commit

---
+++ app/controllers/user_manager_controller.rb
@@ -0,0 +1,4 @@
+class UserManagerController < ApplicationController
+  def index
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'user_manager/index'
+
+  mount ZfUserManager::Engine => '/'
+
+  root 'user_manager#index'
+end
