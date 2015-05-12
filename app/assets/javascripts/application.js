7e7e7c52a904e9f4cfb8433ee4716a66acc03576
Andrea Salomoni <andrea.salomoni@intersail.it>
2015-05-12 10:39:30 +0200

    db setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,5 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-sprockets
+//= require zf_user_manager/zf_user_manager
 //= require_tree .
--- config/initializers/zf_client.rb
+++ config/initializers/zf_client.rb
@@ -1,8 +1,26 @@
 Intersail::ZfClient.configure do |config|
-  # Process instance creation
+  # Process instance
   config.process_uri = "/processes"
-  config.process_base_uri = "http://wrkdev-boncri/ZapFlowWsApi"
-  # User creation
+  config.process_base_uri = "http://wrkdev-jacopo/ZapFlowWsApi"
+  # Process def
+  config.process_def_uri = "/process_defs"
+  config.process_def_base_uri = "http://wrkdev-jacopo/ZapFlowWsApi"
+  # Activity def
+  config.activity_def_uri = "/activity_defs"
+  config.activity_def_base_uri = "http://wrkdev-jacopo/ZapFlowWsApi"
+  # User
   config.user_uri = "/users"
-  config.user_base_uri = "http://wrkdev-boncri/ZapFlowWsApi"
+  config.user_base_uri = "http://wrkdev-jacopo/ZapFlowWsApi"
+  # Unit
+  config.unit_uri = "/units"
+  config.unit_base_uri = "http://wrkdev-jacopo/ZapFlowWsApi"
+  # Role
+  config.role_uri = "/roles"
+  config.role_base_uri = "http://wrkdev-jacopo/ZapFlowWsApi"
+  # Urr
+  config.urr_uri = "/urrs"
+  config.urr_base_uri = "http://wrkdev-jacopo/ZapFlowWsApi"
+  # Acl
+  config.acl_uri = "/acls"
+  config.acl_base_uri = "http://wrkdev-jacopo/ZapFlowWsApi"
 end
